a228c14e61fe8c60b79930909a9f1114849fa89b
redmor1 <lightz17@hotmail.com>
2023-07-04 23:54:16 -0300

    Agregar ruteo y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Categories from "./pages/Categories/Categories";
+import Products from "./pages/Products/Products";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import CreateProduct from "./pages/CreateProduct/CreateProduct";
+import EditProduct from "./pages/EditProduct/EditProduct";
+import CartDetails from "./pages/CartDetails/CartDetails";
+
 function App() {
   return (
-    <>
-      <p className="display-1">hola</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products/create" element={<CreateProduct />} />
+          <Route path="/products/edit/:id" element={<EditProduct />} />
+          <Route path="/cart-detail" element={<CartDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/CartDetails/CartDetails.jsx
@@ -0,0 +1,9 @@
+function CartDetails() {
+  return (
+    <>
+      <h1>CartDetails</h1>
+    </>
+  );
+}
+
+export default CartDetails;
