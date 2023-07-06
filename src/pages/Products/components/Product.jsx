d1171b7e8ba1de0ebd4bede579e30da5f1817361
redmor1 <lightz17@hotmail.com>
2023-07-06 10:45:58 -0300

    Agregar TODOs

--- src/pages/Products/components/Product.jsx
+++ src/pages/Products/components/Product.jsx
@@ -4,7 +4,12 @@ function Product(props) {
   return (
     <div className="card col-3 mx-1 mb-5 border-0">
       <Link to={`/products/${props.id}`} className="btn text-start">
-        <img src={props.image} className="card-img-top rounded-0" alt="..." />
+        <img
+          // TODO: Put a placeholder image if props.image leads to broken image... need to fiddle with status code
+          src={props.image}
+          className="card-img-top rounded-0"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title mt-2 fw-bold">{props.title}</h5>
           <p className="card-text">${props.price}</p>
