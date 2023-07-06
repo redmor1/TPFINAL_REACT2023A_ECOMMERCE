cc4d078873ac56cee44e35d29801c34da775219b
redmor1 <lightz17@hotmail.com>
2023-07-06 09:29:17 -0300

    Crear funcion fetchData

---
+++ src/functions/fetchData.js
@@ -0,0 +1,11 @@
+async function fetchData(API_URL, entityName) {
+  const res = await fetch(`${API_URL}/${entityName}/?offset=0&limit=10`);
+  const json = await res.json();
+  if (json.error) {
+    throw new Error(json.error);
+  }
+
+  return json;
+}
+
+export default fetchData;
