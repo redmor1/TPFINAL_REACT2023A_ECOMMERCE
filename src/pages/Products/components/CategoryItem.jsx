78964df431c4550382bb1803e676c59dac44ceb0
redmor1 <lightz17@hotmail.com>
2023-07-05 22:17:07 -0300

    Agregar estilizado de componente CategoryItem

--- src/pages/Products/components/CategoryItem.jsx
+++ src/pages/Products/components/CategoryItem.jsx
@@ -1,8 +1,18 @@
-function CategoryItem() {
+function CategoryItem(props) {
   return (
-    <>
-      <h3>CategoryItem</h3>
-    </>
+    <li className="list-group-item">
+      <input
+        className="form-check-input me-1"
+        type="checkbox"
+        value=""
+        // TODO: Add a callback function that does something
+        onChange={() => {}}
+        id={props.id}
+      />
+      <label className="form-check-label stretched-link" htmlFor={props.id}>
+        {props.name}
+      </label>
+    </li>
   );
 }
 
