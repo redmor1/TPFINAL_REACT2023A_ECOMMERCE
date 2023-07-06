bbe34b8b9b59d8fac5e215980fe1b265d114982f
redmor1 <lightz17@hotmail.com>
2023-07-06 10:39:00 -0300

    Crear logica para que el componente CategoryItem lea la URL y cambie su estado

--- src/functions/fetchData.js
+++ src/functions/fetchData.js
@@ -1,4 +1,5 @@
 async function fetchData(API_URL, entityName) {
+  // TODO: deal with pagination
   const res = await fetch(`${API_URL}/${entityName}/?offset=0&limit=10`);
   const json = await res.json();
   if (json.error) {
--- src/pages/Products/components/CategoryItem.jsx
+++ src/pages/Products/components/CategoryItem.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 function CategoryItem(props) {
+  const [isChecked, setIsChecked] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const categoryId = searchParams.get("categoryId");
+
+    setIsChecked(categoryId === props.id.toString());
+  }, [location.search, props.id]);
+
+  function handleCheckboxChange() {
+    setIsChecked(!isChecked);
+  }
+
   return (
     <li className="list-group-item">
       <input
         className="form-check-input me-1"
         type="checkbox"
-        value=""
-        // TODO: Add a callback function that does something
-        onChange={() => {}}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
         id={props.id}
       />
       <label className="form-check-label stretched-link" htmlFor={props.id}>
