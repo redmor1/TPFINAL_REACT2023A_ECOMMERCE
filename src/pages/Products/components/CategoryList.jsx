b8a3937ec1ca4b3586baf0312ddd1bd12ceaa749
redmor1 <lightz17@hotmail.com>
2023-07-05 22:18:11 -0300

    Agregar fetch a API con renderizado en componente CategoryList

--- src/pages/Products/components/CategoryList.jsx
+++ src/pages/Products/components/CategoryList.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from "react";
+import { API_URL } from "../../../constants/API_URL";
 import CategoryItem from "./CategoryItem";
 
 function CategoryList() {
+  const [categories, setCategories] = useState();
+
+  // TODO: Abstract this function and state into a custom one so i can use it in two places
+  async function getCategories(API_URL) {
+    try {
+      const res = await fetch(`${API_URL}/categories`);
+      const json = await res.json();
+      setCategories(json);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getCategories(API_URL);
+  });
+
   return (
     <>
-      <h2>CategoryList</h2>
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
+      <h2>Categories</h2>
+      <ul className="list-group">
+        {categories ? (
+          categories.map((category) => {
+            return (
+              <CategoryItem
+                key={category.id}
+                id={category.id}
+                name={category.name}
+              />
+            );
+          })
+        ) : (
+          <h1>Loading</h1>
+        )}
+      </ul>
     </>
   );
 }
