d6d36acc0e283044ca491322c5b976fecb57e944
redmor1 <lightz17@hotmail.com>
2023-07-10 01:22:21 -0300

    Agregar boton en ProductList

--- src/pages/Products/Filter/Filter.jsx
+++ src/pages/Products/Filter/Filter.jsx
@@ -3,9 +3,9 @@ import CategoryList from "./CategoryList";
 function Filter() {
   return (
     <div className="sidebar col-2">
-      <div className="d-flex align-items-center mb-3">
-        <h4 className="fw-semibold filter-title me-3 mb-0 lh-lg">Filter</h4>
-        <p className="filter-clear text-decoration-underline mb-0">
+      <div className="d-flex align-items-center mb-4">
+        <h4 className="fw-semibold filter-title me-3 mb-0 lh-1">Filter</h4>
+        <p className="filter-clear text-decoration-underline mb-0 lh-1">
           Clear filters
         </p>
       </div>
