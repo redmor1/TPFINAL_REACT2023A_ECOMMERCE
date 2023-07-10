b1d8d8231c796643b28365fb91717d9645a7feaa
redmor1 <lightz17@hotmail.com>
2023-07-10 12:13:52 -0300

    Agregar contenido en page Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,31 @@
 function Login() {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div className="container-fluid my-5 mx-auto">
+      <div className="row justify-content-center">
+        <div className="col-8 d-flex flex-column justify-content-center p-4 bg-white">
+          <h1 className="mt-0">Welcome Back</h1>
+          <p>Login with email</p>
+          <input
+            type="email"
+            placeholder="Email"
+            className="form-control form-control-lg"
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            className="form-control form-control-lg"
+          ></input>
+          <div className="d-flex justify-content-between">
+            <div>
+              <input type="checkbox" id="remember-me"></input>
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+
+            <p>Forgot password?</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
