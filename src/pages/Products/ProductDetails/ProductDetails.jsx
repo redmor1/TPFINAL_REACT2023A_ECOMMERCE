8152942f04d7007d723d644edb6f2fcdd8c9ce87
redmor1 <lightz17@hotmail.com>
2023-07-13 12:59:05 -0300

    Eliminar comentarios

--- src/pages/Products/ProductDetails/ProductDetails.jsx
+++ src/pages/Products/ProductDetails/ProductDetails.jsx
@@ -9,7 +9,6 @@ import { useParams } from "react-router-dom";
 function ProductDetails() {
   const { id: productId } = useParams();
 
-  // Colocate state?
   const {
     data: product,
     isLoading,
@@ -18,14 +17,6 @@ function ProductDetails() {
   } = useQuery([QUERY_KEY_PRODUCTS, productId], () => {
     return fetchData(API_URL, location.pathname);
   });
-
-  // TODO: no funciona el error handler cuando la id es incorrecta, el server responde con 400 bad request, el error de peticion si funciona, este no
-  //   {
-  //     "path": "/api/v1/products/100/?offset=0&limit=10",
-  //     "timestamp": "2023-07-11T17:33:24.670Z",
-  //     "name": "EntityNotFoundError",
-  //     "message": "Could not find any entity of type \"Product\" matching: {\n    \"relations\": [\n        \"category\"\n    ],\n    \"where\": {\n        \"id\": 100\n    }\n}"
-  // }
 
   return (
     <>
