7c861bdff39cb8e1d163a9c08c28ac8624663bc1
redmor1 <lightz17@hotmail.com>
2023-07-15 11:30:48 -0300

    Agregar hook fetchUser para conseguir info del user usando el token

---
+++ src/functions/fetchUser.js
@@ -0,0 +1,21 @@
+// hago un fetch al endpoint (SOLO SI EXISTE el access_token dentro del localStorage) y si me retorna un error
+// devuelvo un objeto con isUserValid en false y hago un console.error del error
+// si me retorna un user devuelvo un objeto con isUserValid en true y dentro de data el user
+async function fetchUser(API_URL, pathName, access_token) {
+  if (localStorage.getItem("access_token")) {
+    const res = await fetch(`${API_URL}${pathName}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(access_token).access_token}`,
+      },
+    });
+    if (!res.ok) {
+      console.error(res.statusText);
+      return { isUserValid: false };
+    }
+    const json = await res.json();
+    console.log(json);
+    return { isUserValid: true, data: json };
+  }
+}
+
+export default fetchUser;
