9d29c231f13ccda27961cbfb087b53848bd6b083
redmor1 <lightz17@hotmail.com>
2023-07-31 22:10:35 -0300

    Agregar boton para volver a la pagina principal

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Checkout() {
-  return <h1>Purchase completed successfully</h1>;
+  return (
+    <>
+      <h1>Purchase completed successfully</h1>
+      <Link to={"/"} className="btn btn-primary">
+        Return to home
+      </Link>
+    </>
+  );
 }
 
 export default Checkout;
